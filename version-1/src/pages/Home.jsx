7de0b1b9ae52d90cadf4d0bcbdac3138678d9279
mimiiiren <mimiren015@gmail.com>
2026-01-20 18:34:06 -0500

    added input field and more styling to cards

--- version-1/src/pages/Home.jsx
+++ version-1/src/pages/Home.jsx
@@ -1,16 +1,34 @@
 import CountryCard from "../components/CountryCard";
+import { useState } from "react";
 
 export default function Home({ countriesData }) {
-  const sortedCountries = countriesData.sort((countryA, countryB) =>
-    countryA.name.common.localeCompare(countryB.name.common)
-  );
+  const [input, setInput] = useState("");
+  const filteredCountries = countriesData
+    // implicit return without {}, must be one line
+    .filter((country) =>
+      country.name.common.toLowerCase().includes(input.toLowerCase())
+    )
+    .sort((countryA, countryB) =>
+      countryA.name.common.localeCompare(countryB.name.common)
+    );
+
+  function handleInput(e) {
+    setInput(e.target.value);
+  }
   return (
     <div className="home">
+      <input
+        className="input"
+        type="text"
+        value={input}
+        onChange={handleInput}
+        placeholder="Search for a country..."
+      />
       <div className="CountryCardContainer">
         {/* sort is method to reorder alphabetically by comparison  */}
         {/* compare countryA to B, the lower letter comes first */}
         {/* localeCompare is for strings, special charcters like é */}
-        {sortedCountries.map((item) => {
+        {filteredCountries.map((item) => {
           return <CountryCard country={item} key={item.cca3} />;
         })}
       </div>
