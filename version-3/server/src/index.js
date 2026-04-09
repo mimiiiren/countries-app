399029f8e3663865222d9d6624d6e6e564b2c229
mimiiiren <mimiren015@gmail.com>
2026-04-09 13:38:00 -0400

    remove config file

--- version-3/server/src/index.js
+++ version-3/server/src/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import pg from "pg";
-import config from "./config.js";
+
 
 const db = new pg.Pool({
     connectionString: config.databaseUrl + "&uselibpqcompat=true",
